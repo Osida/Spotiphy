575afa45f6e1caccafc356bfce2630b52240156a
osida <you@example.com>
2020-12-10 04:11:36 -0500

    1st commit to Spotiphy

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    // BEM convention
+    <div className="app">
+      <h1>uwu boy </h1>
+      {/* spotify logo */}
+      {/* login w/ spotify button */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login page</h2>
+      {/* spotify logo */}
+      {/* Login w/ spotify button */}
+    </div>
+  );
+}
+
+export default Login;
